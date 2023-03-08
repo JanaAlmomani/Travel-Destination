025dfc8b5f853ba8065d58d5e71885c1d4d648b6
janaAlmomani <jana2000510@gmail.com>
2023-03-08 11:32:40 +0000

    update

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import'./Header.css';
+function Header() {
+    return (
+        <div id ='heder'>
+            <h1> Travel-Destination </h1>
+        
+        </div>
+    );
+}
+
+export default Header; 
